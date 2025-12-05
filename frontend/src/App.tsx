958fa8fcbc459ce8a0ddcc83edb11388e8cda6ef
Alirizo <adobkin@croc.ru>
2025-12-06 02:55:51 +0500

    ui: implement main and common layouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import 'src/locales/i18n';
 
 import 'simplebar-react/dist/simplebar.min.css';
@@ -17,8 +16,8 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 
 import ReduxProvider from 'src/redux/redux-provider';
 
@@ -26,56 +25,47 @@ import Router from 'src/routes/sections';
 
 import ThemeProvider from 'src/theme';
 
-import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
+import {useScrollToTop} from 'src/hooks/use-scroll-to-top';
 
 import ProgressBar from 'src/components/progress-bar';
 import MotionLazy from 'src/components/animate/motion-lazy';
 import SnackbarProvider from 'src/components/snackbar/snackbar-provider';
-import { SettingsProvider, SettingsDrawer } from 'src/components/settings';
+import {SettingsProvider, SettingsDrawer} from 'src/components/settings';
 
-import { AuthProvider, AuthConsumer } from 'src/auth/context/jwt';
+import {AuthProvider, AuthConsumer} from 'src/auth/context/jwt';
 
 export default function App() {
-  console.log(`
 
-░░░    ░░░ 
-▒▒▒▒  ▒▒▒▒ 
-▒▒ ▒▒▒▒ ▒▒ 
-▓▓  ▓▓  ▓▓ 
-██      ██ 
-  
-  `);
+    useScrollToTop();
 
-  useScrollToTop();
-
-  return (
-    <AuthProvider>
-      <ReduxProvider>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <SettingsProvider
-            defaultSettings={{
-              themeMode: 'light', 
-              themeDirection: 'ltr', 
-              themeContrast: 'default', 
-              themeLayout: 'vertical', 
-              themeColorPresets: 'default', 
-              themeStretch: false,
-            }}
-          >
-            <ThemeProvider>
-              <MotionLazy>
-                <SnackbarProvider>
-                  <SettingsDrawer />
-                  <ProgressBar />
-                  <AuthConsumer>
-                    <Router />
-                  </AuthConsumer>
-                </SnackbarProvider>
-              </MotionLazy>
-            </ThemeProvider>
-          </SettingsProvider>
-        </LocalizationProvider>
-      </ReduxProvider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <ReduxProvider>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <SettingsProvider
+                        defaultSettings={{
+                            themeMode: 'light',
+                            themeDirection: 'ltr',
+                            themeContrast: 'default',
+                            themeLayout: 'vertical',
+                            themeColorPresets: 'default',
+                            themeStretch: false,
+                        }}
+                    >
+                        <ThemeProvider>
+                            <MotionLazy>
+                                <SnackbarProvider>
+                                    <SettingsDrawer/>
+                                    <ProgressBar/>
+                                    <AuthConsumer>
+                                        <Router/>
+                                    </AuthConsumer>
+                                </SnackbarProvider>
+                            </MotionLazy>
+                        </ThemeProvider>
+                    </SettingsProvider>
+                </LocalizationProvider>
+            </ReduxProvider>
+        </AuthProvider>
+    );
 }
